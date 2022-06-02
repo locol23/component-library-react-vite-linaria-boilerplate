f0f676c5d7b7a3849f06803c0044e05b8c8995fe
Yoshitaka Terazawa <locol6622@gmail.com>
2022-06-02 12:07:32 +0900

    chore: Use GitHub Actions

--- examples/nextjs/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import { PrimaryButton } from 'component-library'
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => <PrimaryButton fontSize="1rem" onClick={() => alert('clicked!')} text="Primary Button" />
-
-export default Home
